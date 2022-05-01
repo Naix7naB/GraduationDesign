f33332c4263c5dac70ebdace93eb0945f5720b1b
naix7nab <tedxige@foxmail.com>
2022-05-01 22:50:03 +0800

    feat:Custom SearchBar

--- miniprogram/components/SearchBar/SearchBar.js
+++ miniprogram/components/SearchBar/SearchBar.js
@@ -6,11 +6,24 @@ Component({
   },
 
   /* 组件的属性列表 */
-  properties: {},
+  properties: {
+    placeholder: {
+      type: String,
+      value: '请输入内容',
+    },
+    disabled: {
+      type: Boolean,
+      value: false,
+    },
+  },
 
   /* 组件的初始数据 */
   data: {},
 
   /* 组件的方法列表 */
-  methods: {},
+  methods: {
+    input(e) {
+      this.triggerEvent('search', { value: e.detail.value });
+    },
+  },
 });
